64d00f01424511f7e8712d7d46138864a10b351a
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2022-09-26 17:32:27 -0300

    6 requisito

--- src/App.js
+++ src/App.js
@@ -13,22 +13,69 @@ class App extends React.Component {
       cardAttr2: '',
       cardAttr3: '',
       cardImage: '',
-      cardRare: '',
+      cardRare: 'normal',
       cardTrunfo: false,
       hasTrunfo: false,
       isSaveButtonDisabled: true,
+      savedCards: [],
     };
   }
+
+  buttonValidation = () => {
+    const { cardName, cardDescription, cardImage, cardRare } = this.state;
+    const { cardAttr1, cardAttr2, cardAttr3 } = this.state;
+    const maxSum = 210;
+    const maxValue = 90;
+    const sum = Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3);
+    if (cardName && cardDescription && cardImage && cardRare !== ''
+    && +cardAttr1 > 0 && +cardAttr1 <= maxValue
+    && +cardAttr2 > 0 && +cardAttr2 <= maxValue
+    && +cardAttr3 > 0 && +cardAttr3 <= maxValue) {
+      if (sum <= maxSum) {
+        this.setState({
+          isSaveButtonDisabled: false,
+        });
+      }
+    } else {
+      this.setState({
+        isSaveButtonDisabled: true,
+      });
+    }
+  };
 
   onInputChange = ({ target }) => {
     const { name } = target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
-    this.setState(({
+    this.setState(() => ({
       [name]: value,
-    }));
+    }), this.buttonValidation);
   };
 
   onSaveButtonClick = () => {
+    const { cardName, cardDescription, cardImage, cardRare } = this.state;
+    const { cardAttr1, cardAttr2, cardAttr3, cardTrunfo } = this.state;
+    const newCard = {
+      cardName,
+      cardDescription,
+      cardImage,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardRare,
+      cardTrunfo,
+    };
+    this.setState((prevState) => ({
+      savedCards: [...prevState.savedCards, newCard],
+    }));
+    this.setState(({
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: '0',
+      cardAttr2: '0',
+      cardAttr3: '0',
+      cardImage: '',
+      cardRare: 'normal',
+    }));
   };
 
   render() {
